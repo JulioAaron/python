7e54d762b81314231e65f6ca67e887189a417c6f
JulioAaron <julioaaron@gmail.com>
2019-02-14 18:34:23 -0500

    This is my implementation for the app1 ex Udemy
    Hello this is my implementation for the application 1 in the Udemy course: the-python-mega-course

---
+++ dictionary_recomander.py
@@ -0,0 +1,22 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data.keys():
+        return data[w]
+    elif len(get_close_matches(w, data.keys()))>0:
+        yn = input("Quizo de decir %s? escriba Y para si, o N para no: " % get_close_matches(w, data.keys())[0])
+        if yn=='Y' or yn=='y':
+            return  data[get_close_matches(w, data.keys())[0]]
+        elif yn=='N' or yn=='n':
+            return "La palabra no existe, revise, intente nuevamente"
+        else:
+            return "No se se entiendo su elección Y o N"
+    else:
+        return "La palabra no existe, revise, intente nuevamente"
+
+word = input("Ingrese una palabra: ")
+print(translate(word))
