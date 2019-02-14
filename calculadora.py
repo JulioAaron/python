a05cae44a5fd5a15ea53385e72acf1036854fa52
JulioAaron <julioaaron@gmail.com>
2019-02-13 23:21:56 -0500

    Create calculadora.py
    D1 #100daysOfCode

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+#GNU nano 2.2.6                                   File: calculadora.py                                                                             
+
+n1 = int(input("Ingrese un numero"))
+n2 = int(input("Ingrese otro numero"))
+
+suma = n1 + n2
+resta = n1 + n2
+mult = n1 * n2
+div = float(n1 / n2)
+pot = n1**n2
+
+print("La suma es: ", suma, "La resta: ", resta)
+print("La multiplicacion: ", mult)
+print("La division", div, "La potencia: ", pot)
+
+
+
+
+
+
+
