5716b89071a8bf8f613363a3281c724850088d3f
JulioAaron <julioaaron@gmail.com>
2019-03-27 11:23:33 -0500

    Create HackerRank_d1.py

---
+++ HackerRank_d1.py
@@ -0,0 +1,36 @@
+'''
+Task 
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your variables.
+Use the  operator to perform the following operations: 
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+'''
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+int_variable = 10
+double_variable =20.05
+string_variable = "Hello World!"
+
+# Read and save an integer, double, and String to your variables.
+int_variable = int(input())
+double_variable = float(input())
+string_variable = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i+int_variable)
+
+# Print the sum of the double variables on a new line.
+print(d+double_variable)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+string_variable)
+
+
